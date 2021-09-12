557ce203ce5c9e64c3fd7be9d31ba101aa054aba
theo <me@t3.gg>
2021-09-12 14:53:08 -0700

    Add back animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,4 +3,33 @@ module.exports = {
   mode: "jit",
   purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}"],
   // more options here
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      keyframes: {
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+        "fade-in": {
+          "0%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
+      },
+      animation: {
+        "fade-in-down": "fade-in-down 0.5s ease-out",
+        "fade-in": "fade-in 0.2s ease-out",
+      },
+    },
+  },
 };
