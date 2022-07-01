3d6e660a2fa98cea58aea5f7eac9d799c2ab4506
Jason Docton <Jdocton@gmail.com>
2022-07-01 15:16:37 -0700

    prefetch on internal links, css optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), prefetch()],
 });
