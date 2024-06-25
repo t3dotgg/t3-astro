f737e5b59b1fa2549511656c617b398a74d0955b
Francisco <cinkii@protonmail.com>
2024-06-25 02:47:46 -0300

    update: home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import prefetch from "@astrojs/prefetch";
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [prefetch(), tailwind()],
+  integrations: [prefetch(), tailwind(), react()]
 });
