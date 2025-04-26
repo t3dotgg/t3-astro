e4173ed2d0078e34395ac918f529a2386143b404
Brandon McConnell <brandon@dreamthinkbuild.com>
2025-04-26 15:00:55 -0400

    Update tailwind to v4, replace postcss plugin use with vite plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 import prefetch from "@astrojs/prefetch";
-
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [prefetch(), tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  plugins: [],
+  integrations: [prefetch()],
   redirects: {
     "/links": "/",
     "/faq": "/",
