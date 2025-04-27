8e4f8a372d99573b763a7ae9f912d69af6fd816f
Brandon McConnell <brandon@dreamthinkbuild.com>
2025-04-27 17:12:07 -0400

    Simplify theme toggling (single icon in footer links row)

--- src/assets/themes/index.ts
+++ src/assets/themes/index.ts
@@ -1,3 +1,2 @@
 export { default as LightModeIcon } from "./light.svg.astro";
 export { default as DarkModeIcon } from "./dark.svg.astro";
-export { default as SystemModeIcon } from "./system.svg.astro";
--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -5,30 +5,29 @@ export const getTheme = () => {
   return null;
 };
 
-export function setTheme(theme: 'light' | 'dark' | 'system') {
+export function setTheme(theme: 'light' | 'dark') {
   /**
    * clear the current theme when setting a new theme value
    * (essentially always reset to system theme first)
    */
   window.localStorage.removeItem('theme');
   document.documentElement.classList.remove("light", "dark");
-  if (theme !== 'system') {
-    window.localStorage.setItem('theme', theme);
-    document.documentElement.classList.add(theme);
-  }
+  // now set the new theme
+  window.localStorage.setItem('theme', theme);
+  document.documentElement.classList.add(theme);
 }
 
 export function setUpClientThemeScripts() {
   const startViewTransition = (fn: () => void) => document.startViewTransition?.(fn) ?? fn();
   const setUpThemeScripts = () => {
     // set the initial theme
-    setTheme(getTheme() ?? 'system');
+    setTheme(getTheme());
     // event listener for theme toggling
     document.addEventListener("click", (e) => {
       const target = e.target as HTMLElement;
       if (target?.matches(`button[data-theme-toggle]`)) {
         const theme = target.dataset.themeToggle;
-        if (theme === "light" || theme === "dark" || theme === "system") {
+        if (theme === "light" || theme === "dark") {
           startViewTransition(() => setTheme(theme));
         }
       }
